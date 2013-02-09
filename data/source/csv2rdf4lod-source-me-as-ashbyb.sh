f69043cddd264c11d230c18edd6ff17c2c36cf04
Brendan Ashby <ashbyb@rpi.edu>
2013-02-09 02:49:17 +0000

    updating my URI

--- data/source/csv2rdf4lod-source-me-as-ashbyb.sh
+++ data/source/csv2rdf4lod-source-me-as-ashbyb.sh
@@ -3,7 +3,7 @@
 #3>     <http://purl.org/twc/page/csv2rdf4lod/distributed_env_vars>,
 #3>     <https://github.com/timrdf/csv2rdf4lod-automation/wiki/Script:-source-me.sh> .
 
-export CSV2RDF4LOD_CONVERT_PERSON_URI="http://tw.rpi.edu/web/person/BrendanAshby"
+export CSV2RDF4LOD_CONVERT_PERSON_URI="http://tw.rpi.edu/instances/BrendanAshby"
 export PATH=$PATH`/home/ashbyb/opt/prizms/bin/install/paths.sh`
 export CLASSPATH=$CLASSPATH`/home/ashbyb/opt/prizms/bin/install/classpaths.sh`
 alias semmdd='sudo su semmdd'
